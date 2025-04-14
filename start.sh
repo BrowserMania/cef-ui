35d638efbdc7dd525aa6d8cb53174f4223aaa3ef
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2025-04-14 07:20:02 +0200

    Refactor start.sh to streamline CEF and BitWHIP launch process, removing unnecessary comments and improving startup sequence.

--- start.sh
+++ start.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
 
-# Script pour lancer Chromium Embedded Framework (CEF) en mode headless
+set -e
 
-# Lancons PulseAudio en mode "dummy"
- echo "[+] Lancement du serveur PulseAudio..."
+echo "[+] Lancement de PulseAudio..."
+pulseaudio --start
 
-pulseaudio -D --exit-idle-time=-1
+echo "[+] Démarrage de Xvfb sur :99..."
+Xvfb :99 -screen 0 1920x1080x24 &
+export DISPLAY=:99
+sleep 2
 
-# Lancer l’app Chromium dans CEF (en mode headless mais avec GUI actif)
-echo "[+] Lancement de l'application CEF..."
-xvfb-run --auto-servernum --server-num=99 --server-args="-screen 0 1280x720x24" \
-    --server-args="-ac -s 0 -nolock" \   
-    --auth-file=/tmp/xauth \
-    -- /usr/bin/cefclient --enable-media-stream \
-    --no-sandbox \
-    --disable-gpu \ 
-    --disable-software-rasterizer \
-    --disable-dev-shm-usage \
-    --disable-extensions \
-    --disable-infobars \
-    --disable-web-security \
-    --allow-file-access-from-files \
-    --allow-file-access-from-all-urls \
-    --allow-http-background-page \
-    --allow-insecure-localhost \
-    --user-data-dir=/tmp/cef-data \
-    --disable-application-cache \
-./cef-client &
+echo "[+] Lancement de l’application CEF (cef-ui-simple)..."
+/app/target/release/cef-ui-simple &
+CEF_PID=$!
+sleep 2
 
-# Attendre que l'app se lance, puis capturer l’écran et le son
-sleep 5
+echo "[+] Lancement de BitWHIP en mode streaming..."
+/opt/bitwhip/target/release/bitwhip \
+    stream http://localhost:1337/api/whip browsermania-token &
 
-# ffmpeg capture X11 + Pulse et envoie vers WebRTC ou RTP
-ffmpeg \
-    -f x11grab -video_size 1280x720 -i :99.0 \
-    -f pulse -i default \
-    -vcodec libx264 -preset ultrafast -tune zerolatency \
-    -acodec aac -b:a 128k \
-    -f rtp rtp://YOUR_SFU_HOST:5004
-
+wait $CEF_PID
